657d603e30667d54c89f165c458d76f298f49b07
Anas Seth <m.anasseth00@gmail.com>
2024-05-05 11:59:45 +0500

    Covered Array Filter Function

--- 5-5-2024.ts
+++ 5-5-2024.ts
@@ -22,29 +22,48 @@
 let legend: string[] = ["Ahmed", "Awais", "Hamza"];
 
 console.log("\n Push");
-console.log("Before Push :",legend);
+console.log("Before Push :", legend);
 legend.push("Hina");
-console.log("After Push :",legend);
+console.log("After Push :", legend);
 
 console.log("\n Unshift");
-console.log("Before Unshift :",legend);
+console.log("Before Unshift :", legend);
 legend.unshift("Anas");
-console.log("After Unshift :",legend);
+console.log("After Unshift :", legend);
 
 console.log("\n Pop");
-console.log("Before Pop :",legend);
+console.log("Before Pop :", legend);
 legend.pop();
-console.log("After Pop :",legend);
+console.log("After Pop :", legend);
 
 console.log("\n Shift");
-console.log("Before Shift :",legend);
+console.log("Before Shift :", legend);
 legend.shift();
-console.log("After Shift :",legend);
+console.log("After Shift :", legend);
 
-console.log("\n Filter");
-console.log("Before Filter :",legend);
-legend.filter();
-console.log("After Filter :",legend);
+// console.log("\n Filter");
+// console.log("Before Filter :", legend);
+// let newFilterArray = legend.filter("Hamza");
+// console.log("After Filter :", legend);
+
+
+let sampleArray2 = [1, 2, 4, 5, 7, 8];
+console.log(sampleArray2.filter((a) => a % 2 == 0))
+// item 1 goes in fat arrow func and is false
+// item 2 goes in fat arrat func and is true
+// item 3 goes in fat arrat func and is true
+// item 4 goes in fat arrat func and is false
+// item 5 goes in fat arrat func and is false
+// item 6 goes in fat arrat func and is true
+
+
+
+function test(a) {
+    return a % 2 == 0
+}
+
+(a) => a % 2 == 0;
+
 
 
 
