b86fc9f14acd929c5dea1d2878da305a07e3fc81
Anas Seth <m.anasseth00@gmail.com>
2024-05-05 11:42:16 +0500

    Array Examples Covered

--- 5-5-2024.js
+++ 5-5-2024.js
@@ -1,8 +1,5 @@
 // Hoisting & Temporal Dead Zone
 // https://www.freecodecamp.org/news/javascript-temporal-dead-zone-and-hoisting-explained/
-
-
-
 // Recursive Function
 // function recursiveFunc(count: number) {
 //     if (count >= 0) {
@@ -11,41 +8,26 @@
 //     }
 // }
 // recursiveFunc(5);
-
 // Array     // Index 0, 1, 2, 3, 4, 5, 6,  7,  8,  9,
 // let sampleArray = [1, 2, 3, 4, 5, 6, 45, 46, 67, 98, 1, 5, 90];
 // console.log(sampleArray.length);
 // sampleArray[0];
 // sampleArray[9] = 108;
-
 // Array Method
-let legend: string[] = ["Ahmed", "Awais", "Hamza"];
-
+var legend = ["Ahmed", "Awais", "Hamza"];
 console.log("\n Push");
-console.log("Before Push :",legend);
+console.log("Before Push :", legend);
 legend.push("Hina");
-console.log("After Push :",legend);
-
+console.log("After Push :", legend);
 console.log("\n Unshift");
-console.log("Before Unshift :",legend);
+console.log("Before Unshift :", legend);
 legend.unshift("Anas");
-console.log("After Unshift :",legend);
-
+console.log("After Unshift :", legend);
 console.log("\n Pop");
-console.log("Before Pop :",legend);
+console.log("Before Pop :", legend);
 legend.pop();
-console.log("After Pop :",legend);
-
+console.log("After Pop :", legend);
 console.log("\n Shift");
-console.log("Before Shift :",legend);
+console.log("Before Shift :", legend);
 legend.shift();
-console.log("After Shift :",legend);
-
-console.log("\n Filter");
-console.log("Before Filter :",legend);
-legend.filter();
-console.log("After Filter :",legend);
-
-
-
-
+console.log("After Shift :", legend);
