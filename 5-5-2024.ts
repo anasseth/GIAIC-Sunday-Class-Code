c6e6025e5f11fcbe5c89ff4a8371dde9dd19848c
Anas Seth <m.anasseth00@gmail.com>
2024-05-05 11:22:30 +0500

    Array Basic Example

--- 5-5-2024.ts
+++ 5-5-2024.ts
@@ -11,3 +11,14 @@
 //     }
 // }
 // recursiveFunc(5);
+
+// Array     // Index 0, 1, 2, 3, 4, 5, 6,  7,  8,  9,
+// let sampleArray = [1, 2, 3, 4, 5, 6, 45, 46, 67, 98, 1, 5, 90];
+// console.log(sampleArray.length);
+// sampleArray[0];
+// sampleArray[9] = 108;
+
+
+
+
+
