bb5c04f0e930046917bb88818836bbc422e45743
Anas Seth <m.anasseth00@gmail.com>
2024-05-05 10:29:33 +0500

    Hoisting Article Link Added

--- 5-5-2024.ts
+++ 5-5-2024.ts
@@ -1,7 +1,13 @@
-function recursiveFunc(count: number) {
-    if (count >= 0) {
-        console.log("CountDown : ", count);
-        recursiveFunc(count - 1);
-    }
-}
-recursiveFunc(5);
+// Hoisting & Temporal Dead Zone
+// https://www.freecodecamp.org/news/javascript-temporal-dead-zone-and-hoisting-explained/
+
+
+
+// Recursive Function
+// function recursiveFunc(count: number) {
+//     if (count >= 0) {
+//         console.log("CountDown : ", count);
+//         recursiveFunc(count - 1);
+//     }
+// }
+// recursiveFunc(5);
